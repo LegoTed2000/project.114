a0808a774760f89c7c2a552b57b55bc43a92f3c0
LegoTed2000 <112373501+LegoTed2000@users.noreply.github.com>
2023-08-30 15:55:40 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload() {
+
+}
+
+function setup() {
+    canvas = createCanvas(700, 700);
+    canvas.center();
+}
+
+function draw() {
+
+}
+
+function take_snapshot() {
+    save('Mustash.png');
+}
