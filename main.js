c81ba8e4b5f303dc6dd19620a8956316b76b8feb
LegoTed2000 <112373501+LegoTed2000@users.noreply.github.com>
2023-09-07 16:23:12 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,23 @@
-function preload() {
+MouthX = 0;
+MouthY = 0;
 
+function preload() {
+    mustash = loadImage("https://i.postimg.cc/3RxxsdZN/LTdj-Ljr-Xc.png");
 }
 
 function setup() {
     canvas = createCanvas(700, 700);
     canvas.center();
-    video = createCapture(VIDEO)
-    video.size(700 , 700);
+    video = createCapture(VIDEO);
+    video.size(700, 700);
     video.hide();
-    poseNet = ml5.poseNet(video , modelloaded);
-    poseNet.on("pose", gotposes) 
+    poseNet = ml5.poseNet(video, modelloaded);
+    poseNet.on("pose", gotposes)
 }
 
 function draw() {
     image(video, 0, 0, 700, 700);
+    image(mustash, MouthX -35, MouthY +5, 80, 40);
 }
 
 function take_snapshot() {
@@ -24,10 +28,12 @@ function modelloaded() {
     console.log("MODEL HAS LOADED YAAAAAAYYYYYYYY!!!!")
 }
 
-function gotposes(resuts) {
-    if(resuts.length > 0) {
-        console.log(resuts);
-        console.log("The nose x = " + results[0].pose.nose.x);
-        console.log("The nose y = " + results[0].pose.nose.y);
+function gotposes(results) {
+    if (results.length > 0) {
+        console.log(results);
+        MouthX = results[0].pose.nose.x;
+        MouthY = results[0].pose.nose.y;
+        console.log("The mouth x = " + results[0].pose.nose.x);
+        console.log("The mouth y = " + results[0].pose.nose.y);
     }
 } 
